eddbbfe9deabfb37e14c4fbcbbc301a5bcc0af5c
Kazem Mirzayi <kazem.mirzayi@gmail.com>
2024-09-20 19:26:06 +0200

    changing 20.9.2024

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className="hero">Hero</div>
-  )
-}
+    <div className="hero">
+      <div className="hero-text">
+        <h1>We ensure better education for better world</h1>
+        <p>
+          Our cutting-edge curriculum is designed to empower students with the
+          knowledge, skills and the experiences needed to excel in the dynamic
+          field of education
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
